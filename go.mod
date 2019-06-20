609a65eee4d6dfa34ea39acb4fc4bc39a26b81d2
Jordan Knott <jordan@jordanthedev.com>
2019-06-20 11:14:14 -0500

    add generate & serve cmds

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.12
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/gorilla/mux v1.7.2
+	github.com/spf13/cobra v0.0.5
 )
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/gorilla/mux"
+	"github.com/spf13/cobra"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -27,6 +31,22 @@ type TemplateData struct {
 	Config ConfigData
 }
 
+func generate() {
+	var config ConfigData
+	var renderedTmpl bytes.Buffer
+	if _, err := toml.DecodeFile("config.toml", &config); err != nil {
+		panic(err)
+	}
+	tmpl := template.Must(template.ParseFiles("tmpl/welcome.html"))
+
+	tmpl.Execute(&renderedTmpl, TemplateData{config})
+
+	err := ioutil.WriteFile("index.html", renderedTmpl.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	var config ConfigData
 	if _, err := toml.DecodeFile("config.toml", &config); err != nil {
@@ -37,8 +57,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, TemplateData{config})
 }
 
-func main() {
-
+func Serve() {
 	r := mux.NewRouter()
 
 	// This will serve files under http://localhost:8000/static/<filename>
@@ -52,4 +71,32 @@ func main() {
 	}
 
 	log.Fatal(srv.ListenAndServe())
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "homepage",
+	Short: "generate a index.html using a template and config file",
+	Long:  "generate a index.html using a template and config file",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("generating index.html")
+		generate()
+	},
+}
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "run a web server that render's a template using a config file",
+	Long:  "run a web server that render's a template using a config file",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serving web server on 127.0.0.1:9090")
+		Serve()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+}
+
+func main() {
+	rootCmd.Execute()
 }
